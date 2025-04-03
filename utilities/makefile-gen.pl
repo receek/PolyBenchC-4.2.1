eaa352e55633dc1f357fbd7cf3279efea5745148
receek <krzysztof.juszczyk20@gmail.com>
2025-04-03 19:53:48 +0200

    [makefile] Modify makefile generation script for each kernel

--- utilities/makefile-gen.pl
+++ utilities/makefile-gen.pl
@@ -60,12 +60,19 @@ print FILE << "EOF";
 include $configFile
 
 EXTRA_FLAGS=$extra_flags{$kernel}
+DATASET_SIZE_FLAG = -D\$(DATASET_SIZE)_DATASIZE
 
-$kernel: $kernel.c $kernel.h
-	\${VERBOSE} \${CC} -o $kernel $kernel.c \${CFLAGS} -I. -I$utilityDir $utilityDir/polybench.c \${EXTRA_FLAGS}
+OUT_NAME ?= $kernel\_\$(shell echo \$(DATASET_SIZE) | tr '[:upper:]' '[:lower:]')
+
+\$(OUT_NAME): $kernel.c $kernel.h
+	\${VERBOSE} \${CC} -o \${OUT_NAME} \\
+      $kernel.c \\
+      \${CFLAGS} \\
+      -I. -I$utilityDir $utilityDir/polybench.c \\
+      \${DATASET_SIZE_FLAG} \${EXTRA_FLAGS}
 
 clean:
-	@ rm -f $kernel
+	@ rm -f $kernel $kernel\_*
 
 EOF
 
@@ -80,8 +87,10 @@ if ($GEN_CONFIG) {
 open FILE, '>'.$TARGET_DIR.'/config.mk';
 
 print FILE << "EOF";
-CC=gcc
-CFLAGS=-O2 -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_C99_PROTO
+DATASET_SIZE ?= MEDIUM
+
+CC = clang
+CFLAGS = -O2 -DPOLYBENCH_USE_C99_PROTO -DPOLYBENCH_TIME
 EOF
 
 close FILE;
